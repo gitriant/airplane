dddc6d1f09f692f1324094507912db0e2fad676f
Gitriant <yogitrianto7@gmail.com>
2022-02-02 12:49:09 +0700

    membuat shared variables

---
+++ lib/shared/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = Color(0xff5c40cc);
+Color kBlackColor = Color(0xff1f1449);
+Color kWhiteColor = Color(0xffffffff);
+Color kGreyColor = Color(0xff9698a9);
+Color kGreenColor = Color(0xff0ec3ae);
+Color kRedColor = Color(0xffeb70a5);
+Color kBackgroundColor = Color(0xfffafafa);
+Color kInactiveColor = Color(0xffdbd7ec);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: kBlackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: kWhiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: kGreyColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: kGreenColor);
+TextStyle redTextStyle = GoogleFonts.poppins(color: kRedColor);
+TextStyle purpleTextStyle = GoogleFonts.poppins(color: kPrimaryColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
