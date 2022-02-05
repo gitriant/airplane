ee9f14c2ccc7bbb789d0c63922d8e82c4d9fd34b
Gitriant <yogitrianto7@gmail.com>
2022-02-06 00:47:04 +0700

    slicing home bagian new destinations

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:airplane/shared/theme.dart';
 import 'package:airplane/ui/widgets/destinations_card.dart';
+import 'package:airplane/ui/widgets/destinations_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -90,8 +91,61 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget newDestinations() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: 30, left: defaultMargin, right: defaultMargin, bottom: 130),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'New This Year',
+              style:
+                  blackTextStyle.copyWith(fontSize: 18, fontWeight: semiBold),
+            ),
+            DestinationTile(
+              name: 'Danau Beratan',
+              city: 'Singaraja',
+              imageUrl: 'assets/image_destination6.png',
+              rating: 4.5,
+            ),
+            DestinationTile(
+              name: 'Sydney Opera',
+              city: 'Australia',
+              imageUrl: 'assets/image_destination7.png',
+              rating: 4.7,
+            ),
+            DestinationTile(
+              name: 'Roma',
+              city: 'Italy',
+              imageUrl: 'assets/image_destination8.png',
+              rating: 4.8,
+            ),
+            DestinationTile(
+              name: 'Payung Teduh',
+              city: 'Singapura',
+              imageUrl: 'assets/image_destination9.png',
+              rating: 4.5,
+            ),
+            DestinationTile(
+              name: 'Hill Hey',
+              city: 'Monaco',
+              imageUrl: 'assets/image_destination10.png',
+              rating: 4.7,
+            ),
+            DestinationTile(
+              name: 'Danau Beratan',
+              city: 'Singaraja',
+              imageUrl: 'assets/image_destination6.png',
+              rating: 4.5,
+            ),
+          ],
+        ),
+      );
+    }
+
     return ListView(
-      children: [header(), popularDestinations()],
+      children: [header(), popularDestinations(), newDestinations()],
     );
   }
 }
