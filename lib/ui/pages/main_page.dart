6be0f493e422640cfcef09dd0d0233f8ca01ecd5
Gitriant <yogitrianto7@gmail.com>
2022-02-05 22:43:25 +0700

    slicing home bagian header

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/pages/home_page.dart';
 import 'package:airplane/ui/widgets/custom_bottom_navigation_item.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,10 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget buildContent() {
+      return HomePage();
+    }
+
     Widget customBottomNavigation() {
       return Align(
         alignment: Alignment.bottomCenter,
@@ -36,7 +41,7 @@ class MainPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Stack(children: [Text('Main Page'), customBottomNavigation()]),
+      body: Stack(children: [buildContent(), customBottomNavigation()]),
     );
   }
 }
