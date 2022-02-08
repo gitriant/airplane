8fb1d5eb13450c6021b38ce10c19346606311df8
Gitriant <yogitrianto7@gmail.com>
2022-02-08 15:23:21 +0700

    konfigurasi firebase android

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,12 @@ import 'package:airplane/ui/pages/sign-up-page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
