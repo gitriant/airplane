734d5c4eb685e2be8ea95d63c44a63d74f6c3e3c
Gitriant <yogitrianto7@gmail.com>
2022-02-10 14:45:43 +0700

    menambahkan auth state & user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,19 @@
+import 'package:equatable/equatable.dart';
+
+class UserModel extends Equatable {
+  @override
+  final String id;
+  final String email;
+  final String name;
+  final String hobby;
+  final int balance;
+
+  UserModel(
+      {required this.id,
+      required this.email,
+      this.name = '',
+      this.hobby = '',
+      this.balance = 0});
+
+  List<Object?> get props => [id, email, name, hobby, balance];
+}
