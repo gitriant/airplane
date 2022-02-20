fec13c4e5c77589fbd59a40f7c9b2d327fcaaa09
Gitriant <yogitrianto7@gmail.com>
2022-02-20 17:28:08 +0700

    Format Currency & Selected Seat

--- lib/cubit/seat_cubit.dart
+++ lib/cubit/seat_cubit.dart
@@ -1,11 +1,10 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
 
 class SeatCubit extends Cubit<List<String>> {
   SeatCubit() : super([]);
 
   void selectSeat(String id) {
-    print('prev state :$state');
+    // print('prev state :$state');
     if (!isSelected(id)) {
       state.add(id);
     } else {
@@ -13,12 +12,12 @@ class SeatCubit extends Cubit<List<String>> {
     }
 
     print(state);
-    emit(state);
+    emit(List.from(state));
   }
 
   bool isSelected(String id) {
     int index = state.indexOf(id);
-    print('index $index');
+    // print('index $index');
 
     if (index == -1) {
       return false;
--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -216,7 +216,8 @@ class DetailPage extends StatelessWidget {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => ChooseSeatPage()));
+                                builder: (context) =>
+                                    ChooseSeatPage(destination)));
                       })
                 ],
               ),
